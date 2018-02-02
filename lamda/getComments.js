0c1333bf2a76c365f90f4c42bf5d9111903556f0
Joey Ciechanowicz <joey.ciechanowicz@macmillan.com>
2018-02-02 14:57:08 +0000

    update

--- lamda/getComments.js
+++ lamda/getComments.js
@@ -1,18 +1,19 @@
 const AWS = require("aws-sdk");
 const dynamodb = new AWS.DynamoDB();
-const docClient = new AWS.DynamoDB.DocumentClient();
+const npdynamodb = require("./npdynamodb");
+
+const npd = npdynamodb.createClient(dynamodb);
 
 exports.handler = function (event, ctx, callback) {
+  if (!event.postTitle) {
+    callback('No post title provided', null);
+    return;
+  }
 
-  var params = {
-    TableName: 'comments'
-  };
-
-  docClient.scan(params, function (err, data) {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, data);
-    }
-  });
+  npd()
+    .table('commentz')
+    .where("post-title", event.postTitle)
+    .first()
+    .then(data => callback(null, data.Item.comments))
+    .catch(error => callback(error));
 }
