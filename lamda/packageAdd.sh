ab7464096b097fa17594760532ab94d73f0b2ded
Joey Ciechanowicz <joey.ciechanowicz@macmillan.com>
2018-02-02 15:02:28 +0000

    update

--- lamda/packageAdd.sh
+++ lamda/packageAdd.sh
@@ -1,2 +1,2 @@
 zip -r getLambdaFunc.zip .
-aws lambda update-function-code --function-name getComments --zip-file ./getLambdaFunc.zip
+aws lambda update-function-code --function-name getComments --zip-file fileb://${pwd}/getLambdaFunc.zip
